1729e559ceac26b8f98fbe61e7bfcc5f342ec6a7
kirito5572 <first43013@gmail.com>
2019-09-18 17:13:03 +0900

    update

--- src/main/java/SCP079/Listener/getHoryuBanList.java
+++ src/main/java/SCP079/Listener/getHoryuBanList.java
@@ -23,7 +23,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class getHoryuBanList extends ListenerAdapter {
-    private static int caseNum = 178;
+    private static int caseNum = 179;
     @Override
     public void onReady(ReadyEvent event) {
         TimerTask job = new TimerTask() {
